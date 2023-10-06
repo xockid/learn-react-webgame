37ff917429999de90417435e6728835a112ed39a
xockid <xockid@naver.com>
2023-10-06 15:24:56 +0900

    7.틱택토
    틱택토 공부 내용 추가

---
+++ 7.틱택토/client.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+
+import TicTacToe from './TicTacToe';
+
+ReactDom.createRoot(document.querySelector('#root')).render(<TicTacToe />);
